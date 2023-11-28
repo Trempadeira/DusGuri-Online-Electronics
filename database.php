366f1066335b2a136696e2ad7c2d4b716d52155b
Trempadeira <Danieldvizzotto@hotmail.com>
2023-11-28 10:50:45 -0300

    Database
    Database

---
+++ database.php
@@ -0,0 +1,13 @@
+<?php
+
+//Criando lista de dados
+$database = array(
+    0 => [
+        "user" => "admin",
+        "pass" => "1234",
+    ],
+    1 => [
+        "user" => "donini",
+        "pass" => "@senha",
+    ],
+);
