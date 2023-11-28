8505b16e3082587b4b2bdcf019544186a9123961
Trempadeira <Danieldvizzotto@hotmail.com>
2023-11-28 10:51:01 -0300

    Index da pagina
    index

--- index.php
+++ index.php
@@ -1,74 +1,65 @@
 <?php
-include('conexao.php');
+include "/xampp/htdocs/dus/src/controller/header.php";
 
-if (isset($_POST['usuario']) && isset($_POST['senha'])) {
+//Importação dos arquivos que contém as classes User e Database
+include MODEL . "/user.php";
+include MODEL . "/database.php";
 
-    if (empty($_POST['usuario'])) {
-        echo "Preencha seu usuário";
-    } elseif (empty($_POST['senha'])) {
-        echo "Preencha sua senha";
-    } else {
+//Importando arquivo que verifica se a sessão está "ligada".
+//Caso esteja, redireciona o usuário para a página de perfil
+include CONTROLLER . "/session_on.php";
 
-        $usuario = $mysqli->real_escape_string($_POST['usuario']);
-        $senha = $mysqli->real_escape_string($_POST['senha']);
+//Criando objeto da classe Database
+$db = new Database();
 
-        $sql_code = "SELECT * FROM usuarios WHERE usuario = '$usuario' AND senha = '$senha'";
-        $sql_query = $mysqli->query($sql_code) or die("Falha na execução do código SQL: " . $mysqli->error);
+//Selecionar todos os registros da tabela
+//users
+$listUsers = $db->select(
+    "SELECT * FROM usuarios"
+);
 
-        $quantidade = $sql_query->num_rows;
-
-        if ($quantidade == 1) {
-
-            $usuario = $sql_query->fetch_assoc();
-
-            if (!isset($_SESSION)) {
-                session_start();
-            }
-
-            $_SESSION['id'] = $usuario['id'];
-            $_SESSION['nome'] = $usuario['nome'];
-
-            header("Location: meuPerfil.php");
+if( isset($_POST["user"]) &&
+    isset($_POST["pass"]) ) {
+        //Criar um novo objeto da classe User
+        $user = new User(
+            $_POST["user"],
+            $_POST["pass"]
+        );
+        //Fazendo uso do método de verificação de login
+        if( $user->login() ) {
+            //é apresentado um alert javascript
+            echo "<script> alert('AUTENTICADO! ✅') </script>";
+            //é criada uma sessão com os dados do objeto retornado
+            $_SESSION["user"] = $user->getObject();
+            //e então somos redirecionados para a pagina de perfil
+            //var_dump($_SESSION["user"]);
+            header("Refresh: 0; URL = ".VIEW."/profile.php");
         } else {
-            echo "Falha ao logar! Usuário ou senha incorretos";
+            echo "<script> alert('ACESSO NEGADO! ❌') </script>";
         }
     }
-}
+
+    
 ?>
 
-<!DOCTYPE html>
-<html lang="pt-Br">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    
-    <link rel="stylesheet" type="text/css" href="assets/css/estilo.css">
-
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link rel="shortcut icon" type="imagex/png" href="assets/css/img/Logo Dus.png">
-    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;400&display=swap" rel="stylesheet">
-    
-    <title>DusGuri Online Electronics</title>
-</head>
 <body>
    
     <div id="background"></div>
     <section class="container-login">
-    <div class="login">
+    <div class="login" id="login">
         <div>
-        <img src="assets/css/img/Logo Dus.png" alt="logo">
+        <img src="assets/img/Logo Dus.png" alt="logo">
         </div>
 
         <form method="post">
-            <input type="text" name="usuario" placeholder="Usuario" autofocus>
-            <input type="password" name="senha" placeholder="Senha">
+            <input type="text" name="user" id="user" placeholder="Usuario" autofocus>
+            <input type="password" name="pass" id="pass" placeholder="Senha">
             <input type="submit" value="Entrar">
         </form>
         
-        <p>Ainda nao tem uma conta?<a href="cadastro.php">Crie agora</a></p>
+        <p>Ainda nao tem uma conta?<a href="src/view/cadastro.php">Crie agora</a></p>
     </div>
     </section>
-       <script src="index.js"></script>
+
 </body>
 </html>
